a03aa6b9b52794414f0d6e99af60ff06b1613deb
Samir Nunes da Silva <samir.silva12342@gmail.com>
2024-11-21 13:49:45 -0300

    feat: add connectors folder ; docs: update README.md

--- src/drones_simulation/services/simulation.py
+++ src/drones_simulation/services/simulation.py
@@ -2,7 +2,7 @@ from drones_simulation.config import TCP_DRONE_SIMULATION_CONFIG, SimulationConf
 from drones_simulation.log import logger
 from drones_simulation.models.base import BaseBehavior
 from drones_simulation.services.behaviors import BehaviorRouter
-from drones_simulation.services.connector import TCPConnector
+from drones_simulation.services.connectors import TCPConnector
 
 
 class TCPDroneSimulation:
